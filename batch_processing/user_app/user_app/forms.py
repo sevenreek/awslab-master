6fdb3597ce544cf5045fb6e6fdd79594fc5e9bf8
sevenreek <w.walewski112@gmail.com>
2021-05-01 12:07:49 +0200

    user_app as submodule

--- batch_processing/user_app/user_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ProcessingRequestForm(forms.Form):
-    source_file_name = forms.CharField(label='Source filename', max_length=100)
-    dest_file_name = forms.CharField(label='Destination filename', max_length=100)
-    source_file_name = forms.CharField(label='Source filename', max_length=100)
